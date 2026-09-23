242fd57ea01cdcc62cf9f7b5f2aac59981bc9d33
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-23 22:19:38 +0530

    Create nextgreaterelement.cpp

---
+++ WEEK 4/nextgreaterelement.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> nge(n, -1);
+    stack<int> st;
+
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            nge[i] = st.top();
+        }
+        st.push(arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << nge[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
