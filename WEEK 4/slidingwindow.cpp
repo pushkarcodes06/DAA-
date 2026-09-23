9ab95ec4f84b553694f8dc21b483c0bec9a45b45
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-23 22:21:04 +0530

    Create slidingwindow.cpp

---
+++ WEEK 4/slidingwindow.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    if (!(cin >> n >> k)) return 0;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    deque<int> dq;
+    vector<int> result;
+
+    for (int i = 0; i < n; i++) {
+        if (!dq.empty() && dq.front() == i - k) {
+            dq.pop_front();
+        }
+
+        while (!dq.empty() && arr[dq.back()] <= arr[i]) {
+            dq.pop_back();
+        }
+
+        dq.push_back(i);
+
+        if (i >= k - 1) {
+            result.push_back(arr[dq.front()]);
+        }
+    }
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
